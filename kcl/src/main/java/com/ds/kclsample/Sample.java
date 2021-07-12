abed1a1c8cbf389c6d5680618565c356a616a827
d-smith <doug.smith.mail@gmail.com>
2021-07-12 15:03:57 -0700

    added kcl skeleton project

---
+++ kcl/src/main/java/com/ds/kclsample/Sample.java
@@ -0,0 +1,13 @@
+package com.ds.kclsample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Sample {
+
+    private static final Logger log = LoggerFactory.getLogger(Sample.class);
+    public static void main(String... args) {
+
+       log.info("yo");
+    }
+}
